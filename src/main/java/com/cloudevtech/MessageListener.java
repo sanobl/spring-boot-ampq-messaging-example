7ab99769119d4015bc9a6fda8fea9be994a19866
Karan M <karan@cloudevtech.com>
2017-02-13 12:41:47 +0000

    initial commit

---
+++ src/main/java/com/cloudevtech/MessageListener.java
@@ -0,0 +1,23 @@
+package com.cloudevtech;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageListener {
+
+    private static final Logger log = LoggerFactory.getLogger(MessageListener.class);
+
+    @RabbitListener(queues = Application.QUEUE_GENERIC_NAME)
+    public void receiveMessage(final Message message) {
+        log.info("Received message as generic: {}", message.toString());
+    }
+
+    @RabbitListener(queues = Application.QUEUE_SPECIFIC_NAME)
+    public void receiveMessage(final CustomMessage customMessage) {
+        log.info("Received message as specific class: {}", customMessage.toString());
+    }
+}
